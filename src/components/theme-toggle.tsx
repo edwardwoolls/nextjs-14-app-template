911435d4d5d6de91283c146a3f06e5cc1c8661b1
Edward <127947259+ideebiDev@users.noreply.github.com>
2023-12-12 15:12:45 +0800

    Initial Commit

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+import { Switch } from "@/components/ui/switch";
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const isDarkMode = theme === "dark";
+
+  const toggleTheme = () => {
+    setTheme(isDarkMode ? "light" : "dark");
+  };
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch
+        id="theme-toggle"
+        onCheckedChange={toggleTheme}
+        checked={isDarkMode}
+      />
+    </div>
+  );
+}
